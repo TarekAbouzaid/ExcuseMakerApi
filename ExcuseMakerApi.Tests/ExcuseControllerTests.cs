0adf323e813c607f7bafc6ef48c2acc58ff20eab
KuJo7 <joel.kudiyirickal@outlook.com>
2024-01-07 22:55:21 +0100

    Fix tests

--- ExcuseMakerApi.Tests/ExcuseControllerTests.cs
+++ ExcuseMakerApi.Tests/ExcuseControllerTests.cs
@@ -1,71 +1,91 @@
 using DTO;
 using ExcuseMakerApi.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Models;
 using Moq;
-using Polly.Timeout;
+using Persistance.Interfaces;
 using Services.Interfaces;
+using Services.Services;
 
 namespace ExcuseMakerApi.Tests;
 
 [TestFixture]
 public class ExcuseControllerTests
 {
-    private Mock<IExcuseService> _excuseServiceMock;
-    private ExcuseController _excuseController;
-
-    [SetUp]
-    public void Setup()
+    [Test]
+    public async Task GetExcuseById_ExistingId_ReturnsExcuse()
     {
-        _excuseServiceMock = new Mock<IExcuseService>();
-        _excuseController = new ExcuseController(_excuseServiceMock.Object);
+        // Arrange
+        var mockDatabase = new Mock<IExcuseDatabase>();
+        var mockLogger = new Mock<ILogger<ExcuseService>>();
+        var excuseService = new ExcuseService(mockDatabase.Object, mockLogger.Object);
+        var existingId = 1;
+        var expectedExcuse = new Excuse { Id = existingId };
+
+        mockDatabase.Setup(db => db.GetExcuseById(existingId)).ReturnsAsync(expectedExcuse);
+
+        // Act
+        var result = await excuseService.GetExcuseById(existingId);
+
+        // Assert
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Id, Is.EqualTo(existingId));
     }
 
     [Test]
-    public async Task GetRandomExcuse_WhenServiceThrowsHttpRequestException_ShouldReturnInternalServerError()
+    public async Task GetAllExcuses_EmptyList_ReturnsEmptyList()
     {
         // Arrange
-        _excuseServiceMock.Setup(x => x.GetRandomExcuse(It.IsAny<ExcuseCategory>()))
-            .ThrowsAsync(new HttpRequestException("Simulated HTTP error"));
+        var mockDatabase = new Mock<IExcuseDatabase>();
+        var mockLogger = new Mock<ILogger<ExcuseService>>();
+        var excuseService = new ExcuseService(mockDatabase.Object, mockLogger.Object);
+        IEnumerable<Excuse> emptyList = new List<Excuse>();
+
+        mockDatabase.Setup(db => db.GetAllExcuses()).ReturnsAsync(emptyList);
 
         // Act
-        var result = await _excuseController.GetRandomExcuse(ExcuseCategory.Friends);
+        var result = await excuseService.GetAllExcuses();
 
         // Assert
-        var statusCodeResult = result as StatusCodeResult;
-        Assert.IsNotNull(statusCodeResult);
-        Assert.AreEqual(500, statusCodeResult.StatusCode);
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.Empty);
     }
 
     [Test]
-    public async Task GetRandomExcuse_WhenServiceTimesOut_ShouldReturnInternalServerError()
+    public async Task DeleteExcuseById_ExistingId_DeletedSuccessfully()
     {
         // Arrange
-        _excuseServiceMock.Setup(x => x.GetRandomExcuse(It.IsAny<ExcuseCategory>()))
-            .ThrowsAsync(new TimeoutRejectedException("Timeout occurred"));
+        var mockService = new Mock<IExcuseService>();
+        var excuseController = new ExcuseController(mockService.Object);
+        var existingId = 1;
+
+        mockService.Setup(service => service.DeleteExcuse(existingId)).ReturnsAsync(true);
 
         // Act
-        var result = await _excuseController.GetRandomExcuse(ExcuseCategory.Office);
+        var result = await excuseController.DeleteExcuseById(existingId) as OkObjectResult;
 
         // Assert
-        var statusCodeResult = result as StatusCodeResult;
-        Assert.IsNotNull(statusCodeResult);
-        Assert.AreEqual(500, statusCodeResult.StatusCode);
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Value, Is.EqualTo($"deleted ({existingId})"));
     }
-    
+
     [Test]
-    public async Task GetRandomExcuse_WhenServiceReturnsNull_ShouldReturnNotFound()
+    public async Task GetRandomExcuse_ExistingCategory_ReturnsRandomExcuse()
     {
         // Arrange
-        _excuseServiceMock.Setup(x => x.GetRandomExcuse(It.IsAny<ExcuseCategory>()))
-            .ReturnsAsync((Excuse)null);
+        var mockService = new Mock<IExcuseService>();
+        var excuseController = new ExcuseController(mockService.Object);
+        var existingCategory = ExcuseCategory.Office;
+        var randomExcuse = new Excuse();
+
+        mockService.Setup(service => service.GetRandomExcuse(existingCategory)).ReturnsAsync(randomExcuse);
 
         // Act
-        var result = await _excuseController.GetRandomExcuse(ExcuseCategory.Partner);
+        var result = await excuseController.GetRandomExcuse(existingCategory) as OkObjectResult;
 
         // Assert
-        var statusCodeResult = result as StatusCodeResult;
-        Assert.IsNotNull(statusCodeResult);
-        Assert.AreEqual(404, statusCodeResult.StatusCode);
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.Value, Is.EqualTo(randomExcuse.Text));
     }
 }
